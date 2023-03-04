8f58cdc004d8d441898cb0cb13309bc0aacb509f
classAndrew <classAndrew@users.noreply.github.com>
2023-03-04 15:33:20 -0800

    add material ui

--- mood-tracker/src/components/App.js
+++ mood-tracker/src/components/App.js
@@ -4,12 +4,24 @@ import Dashboard from "./Dashboard";
 import Login from "./Login";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+
 import './App.css';
 
 function App() {
   return (
     <div className="wrapper">
-      <h1></h1>
+      <Container maxWidth="sm">
+      <Box sx={{ my: 2 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Material UI Create React App example
+        </Typography>
+      </Box>
+      </Container>
+
       <BrowserRouter>
         <Routes>
           <Route path="/dashboard" element={<Dashboard />}/>
