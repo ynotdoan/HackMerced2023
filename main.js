ad3d9b6a5bdf16963b3b6762b5af1a2f5fda5446
classAndrew <classAndrew@users.noreply.github.com>
2023-03-03 20:38:01 -0800

    react-app template

--- main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class HelloMessage extends React.Component {
-    render() {
-      return <div>Hello {this.props.name}</div>;
-    }
-  }
-  
-root.render(<HelloMessage name="Taylor" />);
